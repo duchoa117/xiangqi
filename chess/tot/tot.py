6bea74b5c15dc7d093389e8415dfe0267d6d0e25
HoaVD <duchoapc99@gmail.com>
2019-10-01 20:58:01 +0700

    move tot

--- chess/tot/tot.py
+++ chess/tot/tot.py
@@ -3,16 +3,27 @@ from chess import chess
 from point.point import Point
 from chess.tempPoint.tempPoint import TempPoint
 
-
 class Tot(Chess):
     def __init__(self, point, white):
         if(white == 1):
             Chess.__init__(self, point, "T", white)
         elif(white == 0):
             Chess.__init__(self, point, "t", white)
-
-
-
+    def positiveMove(self):
+        # setUp:
+        self.pMove.clear()
+        # Your code:
+        mL = []
+        m1 = self.point + Point(0, 1)
+        mL.append(m1)
+        if(self.point.y > 4):
+            for i in range(-1, 2, 2):
+                mL.append(self.point + Point(i, 0))
+        for m in mL:
+            if ((not self.isTeammatePoint(m)) and m.checkOnBoard()):
+                # add tP in Positive Move:
+                self.pMove.append(m)
+            # _____________________________________
 def createTot():
     for i in range(2):
         for j in range(5):
---
+++ gameIO.py
@@ -0,0 +1,19 @@
+from chess import chess
+from point.point import Point
+
+
+def gameIO():
+    print("Position: ")
+    x = int(input("x = "))
+    y = int(input("y = "))
+    tP = Point(x, y)
+    print("Chess information: ")
+    c = chess.infor(tP)
+    move = input("Move?(y/n)")
+    if(move == "y"):
+        movePositionX = int(input("X: "))
+        movePositionY = int(input("Y: "))
+        c.move(Point(movePositionX, movePositionY))
+    
+
+
