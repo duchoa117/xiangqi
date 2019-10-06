1ab38a52bee3dd60359581788ea88db06250b202
Le Tuan <letuanmhtb@gmail.com>
2019-10-06 21:42:38 +0700

    add move ma

--- chess/ma/ma.py
+++ chess/ma/ma.py
@@ -8,8 +8,32 @@ class Ma(Chess):
     def __init__(self, point, white):
         if(white == 1):
             Chess.__init__(self, point, "M", white)
+            self.primitiveMove= []
+            for i in range (0,9,1):
+                for j in range (0,9,1):
+                    self.primitiveMove.append(Point(i,j))
         elif(white == 0):
             Chess.__init__(self, point, "m", white)
+            self.primitiveMove= []
+            for i in range (0,9,1):
+                for j in range (0,9,1):
+                    self.primitiveMove.append(Point(i,j))
+
+    def positiveMove(self):
+        #setUp:
+        self.pMove.clear()
+        # Your code:
+        for m in self.primitiveMove:
+            if((abs(m.x - self.point.x) == 1 and abs(m.y-self.point.y) == 2) or 
+                (abs(m.x - self.point.x) == 2 and abs(m.y-self.point.y) == 1) ):
+                if(not self.isTeammatePoint(m)):
+                    if(abs(m.x - self.point.x) == 1):
+                        print(Point(self.point.x,self.point.y + (m.y-self.point.y)/abs((self.point.y-m.y))))
+                        if(not (chess.isChessPoint(Point(self.point.x,self.point.y + (m.y-self.point.y)/abs(m.y-self.point.y)  )))):
+                            self.pMove.append(m)
+                    elif (abs(m.x - self.point.x) == 2): 
+                        if(not (chess.isChessPoint(Point(self.point.x + (m.x-self.point.x)/abs(m.x-self.point.x), self.point.y )))):
+                            self.pMove.append(m)
 
 
 
